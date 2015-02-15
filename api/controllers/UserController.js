079a0b8548ac9a75501a4ac25fff5a16a44d133f
Adam Patai <pataiadam@gmail.com>
2015-02-16 00:55:56 +0100

    Admin view for user model finished

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -17,33 +17,58 @@ module.exports = {
         });
     },
     show: function(req, res){
-        User.find().exec(function (err, users) {
+        User.findOne({id: req.query.id}).exec(function (err, user) {
             if(!!err){
               sails.log.error(err);
               res.flash(err);
             }
 
-            res.view({users: users});
+            res.view({user: user});
         });
+    },
+    create: function(req, res){
+        if(req.method==='GET'){
+            res.view();
+        }else{
+            User.create(req.body).exec(function (err, user) {
+                if(!!err){
+                  sails.log.error(err);
+                  res.flash(err);
+                }
+
+                res.redirect('/user/show?id='+user.id);
+            });
+        }
     },
     update: function(req, res){
-        User.find().exec(function (err, users) {
-            if(!!err){
-              sails.log.error(err);
-              res.flash(err);
-            }
+        if(req.method==='GET'){
+            User.findOne({id: req.query.id}).exec(function (err, user) {
+                if(!!err){
+                  sails.log.error(err);
+                  res.flash(err);
+                }
 
-            res.view({users: users});
-        });
+                res.view({user: user});
+            });
+        }else{
+            User.update({id: req.body.id}, req.body).exec(function (err) {
+                if(!!err){
+                  sails.log.error(err);
+                  res.flash(err);
+                }
+
+                res.redirect('/user/show?id='+req.body.id);
+            });
+        }
     },
     delete: function(req, res){
-        User.find().exec(function (err, users) {
+        User.update({id: req.query.id}, {deletedAt: new Date()}).exec(function (err) {
             if(!!err){
               sails.log.error(err);
               res.flash(err);
             }
 
-            res.view({users: users});
+            res.redirect('/user');
         });
     }
 
