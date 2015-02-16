1dae51a740c04737976ea6cdad22283ec26d7a17
Adam Patai <pataiadam@gmail.com>
2015-02-16 11:42:12 +0100

    Rate model and view created

--- api/controllers/RateController.js
+++ api/controllers/RateController.js
@@ -6,5 +6,69 @@
  */
 
 module.exports = {
+    index: function (req, res) {
+        Rate.find().exec(function (err, rates) {
+            if(!!err){
+              sails.log.error(err);
+              res.flash(err);
+            }
 
+            res.view({rates: rates});
+        });
+    },
+    show: function(req, res){
+        Rate.findOne({id: req.query.id}).exec(function (err, rate) {
+            if(!!err){
+              sails.log.error(err);
+              res.flash(err);
+            }
+
+            res.view({rate: rate});
+        });
+    },
+    create: function(req, res){
+        if(req.method==='GET'){
+            res.view();
+        }else{
+            Rate.create(req.body).exec(function (err, rate) {
+                if(!!err){
+                  sails.log.error(err);
+                  res.flash(err);
+                }
+
+                res.redirect('/rate/show?id='+rate.id);
+            });
+        }
+    },
+    update: function(req, res){
+        if(req.method==='GET'){
+            Rate.findOne({id: req.query.id}).exec(function (err, rate) {
+                if(!!err){
+                  sails.log.error(err);
+                  res.flash(err);
+                }
+
+                res.view({rate: rate});
+            });
+        }else{
+            Rate.update({id: req.body.id}, req.body).exec(function (err) {
+                if(!!err){
+                  sails.log.error(err);
+                  res.flash(err);
+                }
+
+                res.redirect('/rate/show?id='+req.body.id);
+            });
+        }
+    },
+    delete: function(req, res){
+        Rate.update({id: req.query.id}, {deletedAt: new Date()}).exec(function (err) {
+            if(!!err){
+              sails.log.error(err);
+              res.flash(err);
+            }
+
+            res.redirect('/rate');
+        });
+    }
 };
