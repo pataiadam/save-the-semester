3a9ee6be98f3816b504a1d9b62c913221c72a3fa
Adam Patai <pataiadam@gmail.com>
2015-03-03 00:17:28 +0100

    Add some tests for Auth and Coach

---
+++ test/lib/request.js
@@ -0,0 +1,27 @@
+var request = require('request');
+var HOST = 'localhost';
+var PORT = 1337;
+
+var address = 'http://'+HOST+':'+PORT+'/';
+
+module.exports = {
+
+    send : function(api, params, callback){
+        request.post({
+            headers: {'content-type' : 'application/json'},
+            url:     address+api,
+            body:    JSON.stringify(params),
+            jar:     true
+        }, function(error, response, body){
+            if(!!error){
+                console.log("ERROR (in test/lib/request.js): ");
+                console.log(error);
+                console.log("BODY: ");
+                console.log(body);
+                return callback(body);
+            }
+            return callback(JSON.parse(body));
+        });
+    }
+
+};
